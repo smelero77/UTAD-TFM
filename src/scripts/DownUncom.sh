abecbae7b3dec89bb482607666d659ad396d6786
smelero77 <sergiomelero77@gmail.com>
2016-12-16 00:33:59 -0800

    commit

---
+++ src/scripts/DownUncom.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+rm /home/sergio/TFM_Ficheros/Flights/*
+rm /home/sergio/TFM_Ficheros/Airports/*
+rm /home/sergio/TFM_Ficheros/Carriers/*
+rm /home/sergio/TFM_Ficheros/PlaneData/*
+wget http://stat-computing.org/dataexpo/2009/{2007..2008}.csv.bz2  -P /home/sergio/TFM_Ficheros/Flights
+wget http://stat-computing.org/dataexpo/2009/airports.csv -P /home/sergio/TFM_Ficheros/Airports
+wget http://stat-computing.org/dataexpo/2009/carriers.csv -P /home/sergio/TFM_Ficheros/Carriers
+wget http://stat-computing.org/dataexpo/2009/plane-data.csv -P /home/sergio/TFM_Ficheros/PlaneData
+cd /home/sergio/TFM_Ficheros/Flights
+bunzip2 *.bz2
